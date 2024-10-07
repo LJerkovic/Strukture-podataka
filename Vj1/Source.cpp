66cee230c223bf876ba6fff67f480b310c0b44dc
LJerkovic <ljerko01@fesb.hr>
2024-10-07 19:47:59 +0200

    PRVI ZADATAK POD A

--- Vj1/Source.cpp
+++ Vj1/Source.cpp
@@ -1,9 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define ERROR_OF 1
+#define MAX 1024
+
+
+int NumberOfRows(const char*);
 
 int main() {
+	 
+	int numOfRows = NumberOfRows("imedat.txt");
 
-	printf("Hello world");
+ 
+	
+
+
+
 	return 0;
 
+
+}
+
+int NumberOfRows(const char*) {
+	FILE *fp = NULL;
+	int numOfRows = 0;
+	char buffer[MAX] = {};
+	fp = fopen(fileName, "r");
+	if (!fp) {
+
+		return -1;
+	}
+	while (!feof(fp)) {
+		fgets(buffer, MAX, fp);
+		numOfRows++;
+	}
 
 }
